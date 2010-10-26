afe6d68cd9c1c953fa8f25a9ab8131fc25a73486
nadlabak <pavel@doshaska.net>
2010-10-26 14:10:45 -0700

    changes for 0.03

--- products/cyanogen_umts_sholes.mk
+++ products/cyanogen_umts_sholes.mk
@@ -29,7 +29,7 @@ endif
 
 PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=FRG22D BUILD_DISPLAY_ID=FRG22D PRODUCT_NAME=umts_sholes TARGET_DEVICE=umts_sholes BUILD_FINGERPRINT=verizon/voles/sholes/sholes:2.2/FRG22D/50454:user/release-keys PRODUCT_BRAND=MOTO_RTEU PRIVATE_BUILD_DESC="voles-user 2.2 FRG22D 50454 release-keys" BUILD_NUMBER=50454 BUILD_UTC_DATE=1278333946 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-umts_sholes.map
+# PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-umts_sholes.map
 
 # Enable Windows Media
 WITH_WINDOWS_MEDIA := true
