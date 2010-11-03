8c40b86beb42820133b009c44dd8360289808873
nadlabak <pavel@doshaska.net>
2010-11-03 07:32:15 -0700

    sh hijack and location proxy injection added to squisher

--- products/cyanogen_umts_sholes.mk
+++ products/cyanogen_umts_sholes.mk
@@ -5,7 +5,11 @@ $(call inherit-product, device/motorola/umts_sholes/umts_sholes.mk)
 $(call inherit-product, vendor/cyanogen/products/common.mk)
 
 # Include GSM stuff
-$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+#$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
 
 #
 # Setup device specific product configuration.
@@ -24,7 +28,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Milestone
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.1.0-RC1-Milestone
+        ro.modversion=CyanogenMod-6.1.0-RC3-Milestone
 endif
 
 PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=FRG22D BUILD_DISPLAY_ID=FRG22D PRODUCT_NAME=umts_sholes TARGET_DEVICE=umts_sholes BUILD_FINGERPRINT=verizon/voles/sholes/sholes:2.2/FRG22D/50454:user/release-keys PRODUCT_BRAND=MOTO_RTEU PRIVATE_BUILD_DESC="voles-user 2.2 FRG22D 50454 release-keys" BUILD_NUMBER=50454 BUILD_UTC_DATE=1278333946 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
