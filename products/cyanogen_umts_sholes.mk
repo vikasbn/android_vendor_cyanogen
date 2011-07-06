dfab62bc067984c6da1258e48eac2a7ede0c3089
Micha LaQua <micha.laqua@gmail.com>
2011-07-06 23:58:56 +0200

    bumb version and moved versionBeacon

--- products/cyanogen_umts_sholes.mk
+++ products/cyanogen_umts_sholes.mk
@@ -31,7 +31,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Milestone
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.1.2-0.09-$(shell date +%y.%m.%d)-Milestone
+        ro.modversion=CyanogenMod-6.1.2-0.10-$(shell date +%y.%m.%d)-Milestone
 endif
 
 PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=R.U.R.1920 BUILD_DISPLAY_ID=R.U.R.1920 PRODUCT_NAME=umts_sholes TARGET_DEVICE=umts_sholes BUILD_FINGERPRINT=MOTO_RTEU/umts_sholes/umts_sholes/sholes:2.2.1/SHOLS_U2_05.26.3/296482885:user/release-keys PRODUCT_BRAND=MOTO_RTEU PRIVATE_BUILD_DESC="umts_sholes-user 2.2.1 SHOLS_U2_05.26.3 296482885 release-keys" BUILD_NUMBER=296482885 BUILD_UTC_DATE=1295422848 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
@@ -42,6 +42,7 @@ PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=R.U.R.1920 BUILD_DISPLAY_ID=R.U.R.1920 
 WITH_WINDOWS_MEDIA := true
 
 PRODUCT_COPY_FILES +=  \
-    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/cyanogen/prebuilt/umts_sholes/versionBeacon:system/versionBeacon
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/umts_sholes
